ec6eadb4f05018dd2018d386552ed1ad516922d5
yzhouchen0426 <78391547+yzhouchen0426@users.noreply.github.com>
2021-04-26 19:42:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,9 @@ import streamlit as st
 
 st.title("Group 2!")
 st.title("DS4B Final Project")
+
+import pandas as pd
+
+# Load file in dataframe format
+df_plant_starches = pd.read_excel("GHG avg by food groups.xlsx","Plant starches")
+st.write(df_plant_starches)
