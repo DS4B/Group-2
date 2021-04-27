543928465b8d18f985207700d5004a6be6f3d2ab
thienbn <britnienguyen@gmail.com>
2021-04-27 17:23:08 -0400

    Update tab.py

--- test/tab.py
+++ test/tab.py
@@ -8,7 +8,7 @@ def main():
   html_temp = """
     <script type='text/javascript' src='https://prod-useast-b.online.tableau.com/javascripts/api/viz_v1.js'></script>
     
-    <div class='tableauPlaceholder' style='width: 1203px; height: 686px;'>    
+    <div class='tableauPlaceholder' style='width: 100%; height: 100%;'>    
     <object class='tableauViz' width='100%' height='1527px' style='display:none;'>
     <param name='host_url' value='https%3A%2F%2Fprod-useast-b.online.tableau.com%2F' /> 
     <param name='embed_code_version' value='3' /> 
