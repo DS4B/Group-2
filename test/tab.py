6ff668ee626b73418269f0d736ffdf05e69a96a1
thienbn <britnienguyen@gmail.com>
2021-04-27 18:31:03 -0400

    Update tab.py

--- test/tab.py
+++ test/tab.py
@@ -9,7 +9,7 @@ def main():
     <script type='text/javascript' src='https://prod-useast-b.online.tableau.com/javascripts/api/viz_v1.js'></script>
     
     <div class='tableauPlaceholder' id='my_map' style='position:relative'>    
-    <object class='tableauViz' width='100%' height='1527px style='display:none;'>
+    <object class='tableauViz' width='100%' height='1527px' style='display:none;'>
     <param name='host_url' value='https%3A%2F%2Fprod-useast-b.online.tableau.com%2F' /> 
     <param name='embed_code_version' value='3' /> 
     <param name='site_root' value='&#47;t&#47;btnn' />
