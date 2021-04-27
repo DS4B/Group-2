66a9187038dcf70f6a46c07fbe3c7b2bc0116b92
thienbn <britnienguyen@gmail.com>
2021-04-27 17:34:22 -0400

    Update tab.py

--- test/tab.py
+++ test/tab.py
@@ -8,8 +8,8 @@ def main():
   html_temp = """
     <script type='text/javascript' src='https://prod-useast-b.online.tableau.com/javascripts/api/viz_v1.js'></script>
     
-    <div class='tableauPlaceholder' style='position:relative'>    
-    <object class='tableauViz' width='100%' height='1527px' style='display:none;'>
+    <div class='tableauPlaceholder' id='my_map' style='position:relative'>    
+    <object class='tableauViz' style='display:none;'>
     <param name='host_url' value='https%3A%2F%2Fprod-useast-b.online.tableau.com%2F' /> 
     <param name='embed_code_version' value='3' /> 
     <param name='site_root' value='&#47;t&#47;btnn' />
@@ -17,7 +17,24 @@ def main():
     <param name='tabs' value='no' />
     <param name='toolbar' value='yes' />
     <param name='showAppBanner' value='false' /></object></div>"
+    
+    var divElement = document.getElementById('my_map');
+    var vizElement = divElement.getElementsByTagName('object')[0];
+    if (divElement.offsetWidth > 800)
+    {
+      vizElement.style.width = '1130px';
+      vizElement.style.height = '727px';
+    } else if (divElement.offsetWidth > 500)
+    {
+      vizElement.style.width = '1130px';
+      vizElement.style.height = '727px';
+    } else {
+      vizElement.style.width = '100%';
+      vizElement.style.height = '1527px';
+    }
+    
   """
+  
   components.html(html_temp)
 
 if __name__ == "__main__":
