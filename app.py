1237cfd340c929fa7736d2ef2c179e7d699c4aa1
yzhouchen0426 <78391547+yzhouchen0426@users.noreply.github.com>
2021-04-30 16:48:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,9 @@ st.title("DS4B Final Project")
 
 import pandas as pd
 
-uploaded_file = st.file_uploader("GHG avg by food groups.xlsx", type="xlsx")
-
-if uploaded_file:
-    df = pd.read_excel(uploaded_file)
-
-    st.dataframe(df)
-    st.table(df)
+df = pd.read_excel("GHG avg by food groups.xlsx")
+st.dataframe(df)
+st.table(df)
 
 # Load file in dataframe format
 #df_plant_starches = pd.read_excel("GHG avg by food groups.xlsx","Plant starches")
