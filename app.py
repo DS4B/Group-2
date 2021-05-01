a6cbf035e4ea465d352baa406418c430217d63c7
yzhouchen0426 <78391547+yzhouchen0426@users.noreply.github.com>
2021-05-01 12:29:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,11 @@ avg_ghg = df.pop('Avg GHG (kg CO2 equivalent/ kg product)')
 df = df.dropna()
 st.dataframe(df)
 
-# Load file in dataframe format
-#df_plant_starches = pd.read_excel("GHG avg by food groups.xlsx","Plant starches")
-st.write("hello")
+
+grouped_df = df.groupby(by=['Food group']).mean()
+grouped_df = grouped_df.sort_values(by = 'Total GHG (kg CO2 equivalent/ kg product)', ascending = False)
+st.write(grouped_df)
+
+# Create bar chart for Total GHG by food group
+f = grouped_df.plot.bar(stacked = True)
+st.pyplot(f)
