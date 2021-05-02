6ff0a5a3b479be39edbae9f93695f361dcd74e49
19jzhang <77645244+19jzhang@users.noreply.github.com>
2021-05-02 11:10:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -33,11 +33,10 @@ total_co2_map = """<script type='text/javascript' src='https://prod-useast-b.onl
 <param name='site_root' value='&#47;t&#47;dsbfinal' /><param name='name' value='Total_CO2_Map&#47;Dashboard1' /><param name='tabs' 
 value='yes' /><param name='toolbar' value='yes' /><param name='showAppBanner' value='false' /></object></div>"""
 
-co2_stacked = """<script type='text/javascript' src='https://prod-useast-b.online.tableau.com/javascripts/api/viz_v1.js'></script><div class='tableauPlaceholder' 
-style='width: 1000px; height: 850px;'><object class='tableauViz' width='1000' height='850' style='display:none;'><param name='host_url' 
-value='https%3A%2F%2Fprod-useast-b.online.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='&#47;t&#47;dsbfinal' 
-/><param name='name' value='StackedBarsCombined&#47;Dashboard1' /><param name='tabs' value='yes' /><param name='toolbar' value='yes' /><param name='showAppBanner' 
-value='false' /></object></div>"""
+co2_stacked = """<script type='text/javascript' src='https://prod-useast-b.online.tableau.com/javascripts/api/viz_v1.js'></script><div class='tableauPlaceholder'style='width: 1280px; height: 563px;'><object class='tableauViz' 
+width='1280' height='563' style='display:none;'><param name='host_url' value='https%3A%2F%2Fprod-useast-b.online.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='&#47;t&#47;dsbfinal' />
+<param name='name'
+value='StackedBarsCombined&#47;Sheet1' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='showAppBanner' value='false' /></object></div>"""
 
 components.html(food_ghg_map, width = 900, height = 500, scrolling = True)
 components.html(total_co2_map, width = 900, height = 500, scrolling = True)
