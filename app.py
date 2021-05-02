5e82b7bd997980399e07e650c8b79a70b21bb8e0
angieutomo <66344040+angieutomo@users.noreply.github.com>
2021-05-02 10:17:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -97,3 +97,119 @@ if selected_metrics == 'Other':
   st.pyplot()
 
 
+
+  st.header("How much food GHG does your meal contribute?")
+
+# Make food_dict a global variable so my functions can have access to it
+global food_dict
+
+food_dict = {'Wheat & Rye': 1.4,
+'Cornmeal': 1.1,
+'Barley': 1.1,
+'Oatmeal': 1.6,
+'Rice': 4,
+'Potatoes': 0.3,
+'Cassava': 0.9,
+'Other Pulses': 1.6,
+'Peas': 0.8,
+'Nuts': 0.2,
+'Groundnuts': 2.4,
+'Soymilk': 1,
+'Tofu': 3,
+'Beef': 59.6,
+'Lamb & Mutton': 24.5,
+'Pork': 7.2,
+'Poultry': 6.1,
+'Eggs': 4.5,
+'Fish (farmed)': 5.1,
+'Shrimps (farmed)': 11.8, 
+'Tomatoes': 1.4,
+'Onions & Leeks': 0.3,
+'Root Vegetables': 0.3,
+'Cabbages': 0.4,
+'Other Vegetables': 0.5,
+'Citrus Fruits': 0.3,
+'Bananas': 0.8,
+'Apples': 0.3,
+'Berries & Grapes': 1.1,
+'Other Fruits': 0.7,
+'Milk': 2.8,
+'Cheese': 21.2, 
+'Wine': 1.4,             
+'Coffee': 16.5,
+'Dark Chocolate': 18.7,
+'Cane Sugar': 2.6,
+'Beet Sugar': 1.4,
+'Soybean Oil': 6,
+'Palm Oil': 7.6,
+'Sunflower Oil': 3.5,
+'Rapeseed Oil': 3.7,
+'Olive Oil': 6,
+'Average plant starch': 1.5,
+'Average plant protein': 1.5,
+'Average animal protein': 17,
+'Average vegetable': 0.6,
+'Average fruit': 0.6,
+'Average plant oils': 5.4,
+}
+
+
+save_list = st.multiselect('Which food items would you like to include?', list(food_dict))
+st.markdown(" ")
+
+
+st.subheader("Use the following conversion cheat sheet to help you estimate the mass of your food items in grams:")
+st.markdown(" ")
+"""- 1 cup of uncooked rice/ beans = 200g
+- 1 cup of uncooked pasta = 100g
+- 1 medium-sized potato = 150g
+- 1 slice of bread = 25g
+- 1 egg = 50g
+- A palm-sized portion of meat = 85g
+- 1 cup of raw non-leafy vegetables = 160g
+- 1 cup of raw leafy vegetables = 80g
+- A handful of berries/ 1 banana/ 1 apple/ 1 orange = 80g
+- 1/3 cup of nuts = 50g
+- 1 cup of milk = 240g
+- 1 tbsp of ground coffee = 5g
+"""
+st.markdown(" ")
+
+
+mass_list = []
+x = 0
+for item in save_list:
+  st.write(save_list[x])
+  mass = st.number_input('How many grams of this item did you consume?', min_value=0, key = str(x))
+  mass_list.append(mass)
+  x += 1  
+st.markdown(" ")
+  
+  
+def getGHG(save_list, mass_list):
+  ghg_list = []
+  for idx,save in enumerate(save_list):  
+    ghg_amount = food_dict[save]
+
+    ghg_calculator = ghg_amount *mass_list[idx]
+    ghg_list.append(ghg_calculator)
+    
+  return ghg_list
+
+ghg_list = getGHG(save_list,mass_list)
+total_ghg = sum(ghg_list)
+st.write(f'These food items contributed {int(total_ghg)} g CO2 equivalent')
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+def func(pct, allvals):
+    absolute = int(round(pct/100.*np.sum(allvals)))
+    return "{:.1f}%\n({:d} g)".format(pct, absolute)
+  
+st.subheader("Your food GHG breakdown:")
+fig = plt.pie(ghg_list, explode=None, labels=save_list, colors=None, autopct=lambda pct: func(pct, ghg_list), shadow=False, radius=0.8)
+st.pyplot()
+st.set_option('deprecation.showPyplotGlobalUse', False)
+      
